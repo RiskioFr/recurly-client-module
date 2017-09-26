51bc28c6094aed23a6e9d235f3f41ad1a4b00721
Nicolas Eeckeloo <neeckeloo@gmail.com>
2017-09-26 22:59:53 +0200

    Add missing return type

--- src/Module.php
+++ src/Module.php
@@ -28,7 +28,7 @@ class Module
         }
     }
 
-    public function getConfig()
+    public function getConfig() : array
     {
         return include __DIR__ . '/../config/module.config.php';
     }

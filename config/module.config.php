0ac407f70f80b638178f2c97d30be63e25a4bc14
Nicolas Eeckeloo <neeckeloo@gmail.com>
2017-10-23 10:59:22 +0200

    Removes useless config

--- config/module.config.php
+++ config/module.config.php
@@ -3,12 +3,6 @@ use Riskio\Recurly\ClientModule\ClientOptions;
 use Riskio\Recurly\ClientModule\Container\ClientOptionsFactory;
 
 return [
-    'recurly' => [
-        'subdomain' => null,
-        'api_key' => null,
-        'private_key' => null,
-    ],
-
     'service_manager' => [
         'factories' => [
             ClientOptions::class => ClientOptionsFactory::class,
